c632a197e077e6f9fa02535394c4a1df1ad47c99
Thanh Vu <thanhauco@gmail.com>
2018-05-22 10:15:00 -0700

    Create Customer entity

--- src/main/java/com/imagine/bank/domain/Customer.java
+++ src/main/java/com/imagine/bank/domain/Customer.java
@@ -1,10 +1,1 @@
-package com.imagine.bank.domain;
-import javax.persistence.*;
-import lombok.Data;
-@Entity @Data
-public class Customer extends BaseEntity {
-    private String firstName;
-    private String lastName;
-    @Column(unique = true)
-    private String email;
-}
+package com.imagine.bank.domain; public class Customer extends BaseEntity { private String firstName; private String lastName; private String email; }
