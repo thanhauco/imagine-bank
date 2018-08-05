97b4317a0f7e9898c82cb6fe29df0cc2dae48d65
Thanh Vu <thanhauco@gmail.com>
2018-08-05 10:45:00 -0700

    Add TransactionRepository

--- src/main/java/com/imagine/bank/repository/TransactionRepository.java
+++ src/main/java/com/imagine/bank/repository/TransactionRepository.java
@@ -1,4 +1,1 @@
-package com.imagine.bank.repository;
-import com.imagine.bank.domain.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {}
+package com.imagine.bank.repository; import com.imagine.bank.domain.Transaction; import org.springframework.data.jpa.repository.JpaRepository; public interface TransactionRepository extends JpaRepository<Transaction, Long> {}
