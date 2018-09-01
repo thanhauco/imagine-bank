ca7ddb87fcc88cbe71db968cc99b1f083f367452
Thanh Vu <thanhauco@gmail.com>
2018-09-01 09:00:00 -0700

    Create AccountController

--- src/main/java/com/imagine/bank/web/AccountController.java
+++ src/main/java/com/imagine/bank/web/AccountController.java
@@ -1,14 +1,1 @@
-package com.imagine.bank.web;
-import com.imagine.bank.service.AccountService;
-import org.springframework.web.bind.annotation.*;
-@RestController
-@RequestMapping(""/api/accounts"")
-public class AccountController {
-    private final AccountService service;
-    public AccountController(AccountService service) { this.service = service; }
-    
-    @GetMapping(""/{id}/balance"")
-    public Object getBalance(@PathVariable String id) {
-        return service.getBalance(id);
-    }
-}
+package com.imagine.bank.web; import com.imagine.bank.service.AccountService; import org.springframework.web.bind.annotation.*; @RestController @RequestMapping("/api/accounts") public class AccountController { private final AccountService service; public AccountController(AccountService s) { this.service=s; } @GetMapping("/{id}/balance") public Object getBalance(@PathVariable String id) { return service.getBalance(id); } }
