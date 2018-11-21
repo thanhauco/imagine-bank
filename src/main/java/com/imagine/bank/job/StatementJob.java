d404b1fc45cfc70624c1e71bc5e1547be3f8443b
Thanh Vu <thanhauco@gmail.com>
2018-11-20 16:00:00 -0800

    Add Scheduled tasks for statements

--- src/main/java/com/imagine/bank/job/StatementJob.java
+++ src/main/java/com/imagine/bank/job/StatementJob.java
@@ -1,10 +1,1 @@
-package com.imagine.bank.job;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-@Component
-public class StatementJob {
-    @Scheduled(cron = ""0 0 0 1 * ?"")
-    public void generateStatements() {
-        // Logic
-    }
-}
+package com.imagine.bank.job; import org.springframework.scheduling.annotation.Scheduled; import org.springframework.stereotype.Component; @Component public class StatementJob { @Scheduled(cron="0 0 0 1 * ?") public void run() {} }
