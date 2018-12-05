ddbb74840b79061ddb9ac94f8f16230e1e66f5bc
Thanh Vu <thanhauco@gmail.com>
2018-12-05 10:00:00 -0800

    Add Investment domain

--- src/main/java/com/imagine/bank/domain/Investment.java
+++ src/main/java/com/imagine/bank/domain/Investment.java
@@ -1,8 +1,1 @@
-package com.imagine.bank.domain;
-import javax.persistence.*;
-import lombok.Data;
-@Entity @Data
-public class Investment extends BaseEntity {
-    private String symbol;
-    private int quantity;
-}
+package com.imagine.bank.domain; import javax.persistence.*; import lombok.Data; @Entity @Data public class Investment extends BaseEntity { private String symbol; private int quantity; }
